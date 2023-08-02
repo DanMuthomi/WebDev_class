425a0fa550d2f3f1b08cb4cf4f4f6eb5499257b7
Daniel Muthomi <thedanielmuthomi@gmail.com>
2023-08-02 23:25:19 +0300

    new database

---
+++ db/class(1).sql
@@ -0,0 +1,135 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 02, 2023 at 10:23 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `class`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cat2`
+--
+
+CREATE TABLE `cat2` (
+  `userid` int(10) NOT NULL,
+  `name` varchar(75) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cat2`
+--
+
+INSERT INTO `cat2` (`userid`, `name`, `email`, `password`) VALUES
+(2, 'abc3', 'abc@abc.com', '$2y$10$NbxgYmMqiLDebgUmu6M/5uqjfKmtDGYxj5drz/BPTGcDG1dk4hvKS');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration`
+--
+
+CREATE TABLE `registration` (
+  `userid` int(3) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `regMonth` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `registration`
+--
+
+INSERT INTO `registration` (`userid`, `username`, `regMonth`) VALUES
+(1, 'Daniel', 'January'),
+(3, 'btrghthbrd', 'January');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`) VALUES
+(1, 'lee@gmail.com', '$2y$10$II9qUFuXhXMylit/fNFm4eBsLsmN8gArMA4V2tlR72vuGN5zPu7/C'),
+(2, 'joe@gmail.com', '$2y$10$PodDxteU6H1yRdnnr8C4mOaCbSaSl3ex5TgrW4hBuOMKU/DZhQaLq'),
+(3, 'abi@gmail.com', '$2y$10$jrxuHhZeg0PjsEzQHWMUAe3pG3yCJnicYZol9Uwk.FCb0XtcgDCE2'),
+(5, '123@123.com', '$2y$10$ewpv./2grYnhadoEyoro/O8Q8XEppCMTtI7ExxbpZMyI2wEYIWY0u'),
+(6, 'wii@wii.com', '$2y$10$Ia156GFwZq1tqoi5PSnYgOmYtGeukBq1vylOZ4t7GwIEnuDuDWyxy');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cat2`
+--
+ALTER TABLE `cat2`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- Indexes for table `registration`
+--
+ALTER TABLE `registration`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cat2`
+--
+ALTER TABLE `cat2`
+  MODIFY `userid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `registration`
+--
+ALTER TABLE `registration`
+  MODIFY `userid` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
